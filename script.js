55d2834bdf3d759bbd760ad56b83a8091992442c
codebyte156 <rahulraut007123@gmail.com>
2024-07-20 19:54:03 +0530

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+let leaf2 = document.getElementById('leaf2');
+
+const maxScrollHeight = 500; // Set your maximum scroll height here
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    if (value > maxScrollHeight) {
+        value = maxScrollHeight;
+    }
+
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * 1 + 'px';
+    
+});
